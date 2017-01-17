f7bd9521b51b8ef9773cf30bbc955267cb274111
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-01-17 00:40:42 +0000

    added the requestURL value

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,13 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+var GITHUB_USER = "felips70";
+var GITHUB_TOKEN = "679bbc02e4e7d7257a37d47186c08f9fc9a0082b";
 
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  return requestURL;
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
